495dcc6a5b5d1e2a690c7bbd859f37a6d1a7085c
Kirishi Hackerspace <sw3n@mail.ru>
2019-02-13 17:42:48 +0300

    add bidlokod
    add bidlokod flags

--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <?php
 
 $text = $_POST['text'];
+$say = $_POST['say'];
 
-function tts_say($text){
+if($say=="SAY"){$send=1;}else{$send=0;}
 
+function tts_say($text,$send){
+  if($send==0){return "";}
   //Место для фильтра входящих данных =)
-
-$cmd = 'wget -q -U Mozilla -O - "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q='.$text.'&tl=Ru-gb"|madplay -';  
-  
-system($text);
+  $cmd = 'wget -q -U Mozilla -O - "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q='.$text.'&tl=Ru-gb"|madplay -';  
+  system($text);
 return "ok";
-
 }
-
 
 echo "
 <html>
-<head><title>PHPTTS</title></head>
+  <head>
+  <title>PHPTTS</title>
+  </head>
 
 <body>
-
-<center>
-<form action='' method='post'>
-<input type='text' name='text'>
-<br/>
-<input type='submit' value='SAY'>
-</form>
-</center>
+  <center>
+    ".tts_say($text,$send)."
+    <form action='' method='post'>
+      <input type='text' name='text'>
+      <br/>
+      <input type='submit' name='say' value='SAY'>
+    </form>
+  </center>
 
 </body>
 </html>
 "
-
 
 ;?>
