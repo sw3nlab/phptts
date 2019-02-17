b73831daa4420f711f905544d20d5cfe46f7d862
cyber <cyber@nettop>
2019-02-17 21:53:09 +0300

    global update

--- index.php
+++ index.php
@@ -5,20 +5,81 @@ $text = $_POST['text'];
 $say = $_POST['say'];
 
 
+$cfg_player = $_POST['cfg_player'];
+$cfg_color = $_POST['cfg_color'];
+$cfg_save = $_POST['cfg_save'];
+$cfg = file("config.txt");
+
+$stream_url = $_POST['stream_url'];
+$stream_save = $_POST['stream_save'];
+$stream_cfg = file("radio.txt");
+
 if($say=="SAY"){$send=1;}else{$send=0;}
 
-function tts_say($text,$send){
+function tts_say($text,$send,$cfg){
   if($send==0){return "";}
 
-	$s1 = "d2dldCAtcSAtVSBNb3ppbGxhIC1PIC0gImh0dHA6Ly90cmFuc2xhdGUuZ29vZ2xlLmNvbS90cmFuc2xhdGVfdHRzP2llPVVURi04JnRvdGFsPTEmaWR4PTAmdGV4dGxlbj0zMiZjbGllbnQ9dHctb2ImcT0=";
-	$s2 = "JnRsPVJ1LWdiInxtYWRwbGF5IC0=";
-	$cmd = base64_decode($s1).''.str_replace('`','',trim($text)).''.base64_decode($s2);
+$s1 = "d2dldCAtcSAtVSBNb3ppbGxhIC1PIC0gImh0dHA6Ly90cmFuc2xhdGUuZ29vZ2xlLmNvbS90cmFuc2xhdGVfdHRzP2llPVVURi04JnRvdGFsPTEmaWR4PTAmdGV4dGxlbj0zMiZjbGllbnQ9dHctb2ImcT0=";
+$s2 = "JnRsPVJ1LWdiIg==";
 
-system($cmd);
+  $cmd = base64_decode($s1).''.str_replace('`','',trim($text)).''.base64_decode($s2).'|'.trim($cfg[1]).' -';
 
-return "ok";
+//system($cmd);
+
+return $cmd;
+
 }
 
+
+function cfg_save($cfg_player,$cfg_color,$cfg_save){
+
+if(!$cfg_save=="save"){return "";}
+
+$buf = "[main]\r\n".htmlspecialchars(trim($cfg_player))."\r\n".htmlspecialchars(trim($cfg_color))."\r\n";
+
+$fp = fopen("config.txt","w");
+fwrite($fp,$buf);
+fclose($fp);
+
+return "<script>document.write('save complete !');document.location.href='?act=cfg';</script>";
+
+}
+
+
+function stream_save($stream_url,$stream_save,$stream_cfg){
+    if(!$stream_save=="save"){return "";}
+
+    $list = '';
+
+    for($i=0;$i<count($stream_cfg);$i++){
+    $list.=$stream_cfg[$i];
+    }
+
+    $list.=$stream_url."\r\n";
+
+    $fp = fopen("radio.txt","w");
+    fwrite($fp,$list);
+    fclose($fp);
+
+    return "<script>document.write('Saved !');document.location.href='?act=radio';</script>";
+
+}
+
+function stream_view($stream_cfg){
+
+    $str = "";
+    for($i=1;$i<count($stream_cfg);$i++){
+    $str.="<a href=''>PLAY</a>".str_replace("\r\n","<br/>",$stream_cfg[$i])."<a href='?act=radio&delete=".$i."'>X</a><br/>";
+    }
+return $str;
+}
+
+
+function stream_delete($stream_cfg,$stream_del_id){
+
+    //Допилю потом =)))
+
+}
 
 
 switch($act){
@@ -26,7 +87,17 @@ switch($act){
 case "radio":
 	$content = "
 		<center>
-			<h3>Radio Stations List</h3>
+            <h3>Radio Stations List</h3>
+".stream_view($stream_cfg)."
+<br/>
+".stream_save($stream_url,$stream_save,$stream_cfg)."
+<form action='' method='post'>
+Radio Stream URL:
+<br/>
+<input type='text' name='stream_url'>
+<input type='hidden' name='stream_save' value='save'>
+<input type='submit' value='Добавить'>
+</form>
 		</center>";
 break;
 
@@ -40,7 +111,7 @@ case "say":
 		      <input type='text' name='text'>
 		      <input type='submit' name='say' value='SAY'>
 			<br/>
-			".tts_say($text,$send)."
+			".tts_say($text,$send,$cfg)."
 		    </form>
 		  </center>
 ";
@@ -49,6 +120,26 @@ break;
 case "alarm":
 $content = "<b>Функция в разработке ;/</b>";
 break;
+
+case "cfg":
+    $content = "
+<center>
+<br/><br/>
+<b>Настройки</b>
+<br/>
+".cfg_save($cfg_player,$cfg_color,$cfg_save)."
+<br/>
+<form action='' method='post'>
+Player:<input type='text' name='cfg_player' value='".$cfg[1]."'>
+<br/><br/>
+Color :<input type='text' name='cfg_color' value='".$cfg[2]."'>
+<br/><br/>
+<input type='hidden' name='cfg_save' value='save'>
+<input type='submit' value='Сохранить'>
+</form>
+</center>
+";
+break;    
 
 default:
 	$content= "
@@ -89,6 +180,8 @@ echo "
 <a href='?act=say'>Голосовое сообщение</a>
 <br/><br/>
 <a href='?act=alarm'>Будильник</a>
+<br/><br/>
+<a href='?act=cfg'>Настройки</a>
 </td><br/>
 <td valign='top'>
 ".$content."
